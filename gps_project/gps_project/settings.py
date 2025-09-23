f35c45a869abb895be5b4a66798a00f0f545ef88
saisah02 <saisah2002@gmail.com>
2025-09-23 10:57:35 +0800

    run-ss

--- gps_project/gps_project/settings.py
+++ gps_project/gps_project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+=6_zh++u5nnsnn14=1j(fg35$mq$@=fizl_ej709n(4@c$qmy'
+SECRET_KEY = 'dynatest'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
